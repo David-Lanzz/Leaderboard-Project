e611460c2c014407d79ded45d1d1493627719481
Olanrewaju Oduola <111298026+David-Lanzz@users.noreply.github.com>
2023-03-06 15:09:38 -0800

    Create leaderboard UI

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './style.css';
+import defaultExport from '../modules/class.js';
+
+const scoresList = document.querySelector('.scoresList');
+window.addEventListener('load', () => {
+  scoresList.innerHTML = defaultExport.addListToDom();
+});
